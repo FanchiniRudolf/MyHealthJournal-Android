7cbc74b65823f87bd3aa7c5801fae27a1a3d285c
rtperezyera <tllezroberto9@gmail.com>
2020-04-18 19:12:56 -0500

    Add Entry overview screen and individual entry layout

---
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/OverviewEntradas.kt
@@ -0,0 +1,12 @@
+package mx.lifehealthsolutions.myhealthjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewEntradas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview_entradas)
+    }
+}
