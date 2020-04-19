7219aeb0bbb763b569d354dc1ecf51efaff2f908
rtperezyera <tllezroberto9@gmail.com>
2020-04-18 19:55:36 -0500

    Add CrearEntrada activity

---
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/CrearEntrada.kt
@@ -0,0 +1,12 @@
+package mx.lifehealthsolutions.myhealthjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearEntrada : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_entrada)
+    }
+}
