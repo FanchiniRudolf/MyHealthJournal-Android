c47757f7855b9a9568f438f095917a1e91cfded0
rtperezyera <tllezroberto9@gmail.com>
2020-04-19 21:40:33 -0500

    Add renglones that recycler views will use, and activity for new entry

---
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/NuevaEntradaAsma.kt
@@ -0,0 +1,12 @@
+package mx.lifehealthsolutions.myhealthjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevaEntradaAsma : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nueva_entrada_asma)
+    }
+}
