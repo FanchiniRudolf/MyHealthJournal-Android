fc34009d30c7337113ffaf4e52f69906ad7b7aeb
Irving <irvingfa72@gmail.com>
2020-04-19 20:13:06 -0500

    Fragments

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/MainActivity.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/MainActivity.kt
@@ -2,11 +2,22 @@ package mx.lifehealthsolutions.myhealthjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import mx.lifehealthsolutions.myhealthjournal.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Fragmento inicial
+        val fragHome = HomeFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.contenedorFragmentos, fragHome)
+            .commit()
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
     }
 }
