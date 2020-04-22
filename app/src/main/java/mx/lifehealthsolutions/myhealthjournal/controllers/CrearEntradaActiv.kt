4d8911860b167be5efd641bcfd8952edc8053421
rtperezyera <tllezroberto9@gmail.com>
2020-04-22 13:37:00 -0500

    Rename activity to create an entry

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/CrearEntradaActiv.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/CrearEntradaActiv.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.lifehealthsolutions.myhealthjournal.R
 
-class CrearEntrada : AppCompatActivity() {
+class CrearEntradaActiv : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
