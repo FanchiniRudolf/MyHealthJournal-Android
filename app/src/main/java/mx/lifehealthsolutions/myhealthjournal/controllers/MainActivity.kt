5867fd0ab77b2efb77f68859f232304c41a39336
rtperezyera <tllezroberto9@gmail.com>
2020-04-22 12:34:01 -0500

    Add logic to change fragment

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/MainActivity.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/MainActivity.kt
@@ -2,6 +2,7 @@ package mx.lifehealthsolutions.myhealthjournal.controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import mx.lifehealthsolutions.myhealthjournal.R
 
@@ -23,9 +24,48 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         navView.setOnNavigationItemSelectedListener{ menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.contenedorFragmentos, fragHome)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+                R.id.navigation_reminders -> {
+                    val fragReminders = ReminderFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.contenedorFragmentos, fragReminders)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+                R.id.navigation_diary -> {
+                    val fragDiary = DiaryFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.contenedorFragmentos, fragDiary)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+                R.id.navigation_about -> {
+                    val fragAbout = AboutFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.contenedorFragmentos, fragAbout)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+                else ->
+                    println("esto no deberia pasar salu2")
+            }
             
 
-            true ^setOnNavigationItemSelectedListener
+            true
         }
 
 
