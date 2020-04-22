ed4065a4347b241461c0fc549487720df707ca6c
rtperezyera <tllezroberto9@gmail.com>
2020-04-22 15:29:17 -0500

    Try to implement starting new activity from fragment

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/DiaryFragment.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/DiaryFragment.kt
@@ -2,6 +2,7 @@ package mx.lifehealthsolutions.myhealthjournal.controllers
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_diary.*
+import kotlinx.android.synthetic.main.fragment_diary.view.*
 import mx.lifehealthsolutions.myhealthjournal.R
 import mx.lifehealthsolutions.myhealthjournal.interfaces.ListenerRecycler
 import mx.lifehealthsolutions.myhealthjournal.models.AdapterViewCondition
@@ -32,6 +34,16 @@ class DiaryFragment : Fragment(), ListenerRecycler {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_diary, container, false);
         configurarRecycler()
+
+
+        rootView.btnNewEvent.setOnClickListener { rootView ->
+            //Log.w("btnNewEvent", "selected")
+            //println("asuuu presionaste el boton")
+            val intCrearEntrada = Intent(activity, CreateEntryActiv::class.java)
+            startActivity(intCrearEntrada)
+        }
+
+
         return inflater.inflate(R.layout.fragment_diary, container, false)
     }
     private fun configurarRecycler() {
