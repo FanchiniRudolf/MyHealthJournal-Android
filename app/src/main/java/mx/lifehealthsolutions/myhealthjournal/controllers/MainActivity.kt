e725e83f97d3e57680a99a3a9671e5b09c72dc14
FanchiniRudolf <fanchinirudolf@gmail.com>
2020-04-22 15:30:12 -0500

    download climate data

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/MainActivity.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/MainActivity.kt
@@ -1,24 +1,32 @@
 package mx.lifehealthsolutions.myhealthjournal.controllers
 
+import android.Manifest
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
 import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_home.*
 import mx.lifehealthsolutions.myhealthjournal.R
 import mx.lifehealthsolutions.myhealthjournal.models.Condition
 import mx.lifehealthsolutions.myhealthjournal.models.User
+import org.json.JSONObject
 
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     private val GPS_PERMIT: Int = 200
     private lateinit var gps: LocationManager
@@ -92,6 +100,50 @@ class MainActivity : AppCompatActivity() {
         configureGPS()
     }
 
+    override fun onResume() {
+        super.onResume()
+        //Check for permit
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED){
+            //have permit allowed
+            gps.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0f, this)
+            onLocationChanged(gps.getLastKnownLocation(LocationManager.GPS_PROVIDER))
+        } else{
+            //ask for permit
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), GPS_PERMIT)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gps.removeUpdates(this)
+    }
+
+
+    private fun setClimate() {
+        val latitud = position.latitude
+        val longitude = position.longitude
+        val address = "https://api.waqi.info/feed/geo:$latitud;$longitude/?token=c77ba1b4e5441ffaaec07a8cb58efc3307bdf850"
+        AndroidNetworking.get(address)
+            .build()
+            .getAsJSONObject(object: JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject?) {
+                    val  aqi = response?.getJSONObject("data")?.getString("aqi")
+                    val pmo10 = response?.getJSONObject("data")?.getJSONObject("iaqi")?.getJSONObject("pm10")?.getString("v")
+                    textView15.setText("pmo10: $pmo10, aqi: $aqi")
+
+                }
+
+                override fun onError(anError: ANError?) {
+                    println("******************************************************")
+                    println("error")
+                    println("******************************************************")
+                }
+
+            })
+    }
+
     private fun configureGPS() {
         //create sensor gps admin
         gps = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -118,6 +170,38 @@ class MainActivity : AppCompatActivity() {
             })
         val alert = dialoge.create()
         alert.show()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        if(location != null){
+            position = location
+            setClimate()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == GPS_PERMIT && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Granted permit
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    //have permit allowed
+                    gps.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0f, this)
+                    onLocationChanged(gps.getLastKnownLocation(LocationManager.GPS_PROVIDER))
+                }
+            } else {
+                //Show Message on how to do it
+            }
+        }
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+    }
+
+    override fun onProviderDisabled(provider: String?) {
     }
 
 }
