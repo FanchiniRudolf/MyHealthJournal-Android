98b66e251a0ee38d28d0524eda6ab7f786f0059d
FanchiniRudolf <fanchinirudolf@gmail.com>
2020-05-13 13:15:36 -0500

    start work on adapter

--- app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/EntryDataActiv.kt
+++ app/src/main/java/mx/lifehealthsolutions/myhealthjournal/controllers/EntryDataActiv.kt
@@ -2,12 +2,18 @@ package mx.lifehealthsolutions.myhealthjournal.controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_crear_entrada.*
 import mx.lifehealthsolutions.myhealthjournal.R
+import mx.lifehealthsolutions.myhealthjournal.models.User
 
 class EntryDataActiv : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry_data)
+        val spinner:Spinner = findViewById(R.id.spinnerTipo)
+        val adapter = User.downloadConditionNames(this)
+        spinnerTipo.adapter = adapter
     }
 }
